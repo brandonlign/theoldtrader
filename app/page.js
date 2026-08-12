9594d9c4695fe18a6feac296dd9e0c75f11d5a25
Brandon Li <brandon.li.gn@gmail.com>
2026-08-11 23:33:52 -0400

    Surface crypto analytics below the trading desk

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
+import CryptoAnalytics from "./crypto-analytics";
 import Dashboard from "./dashboard";
 
 export default function HomePage() {
-  return <Dashboard />;
+  return (
+    <>
+      <Dashboard />
+      <CryptoAnalytics />
+    </>
+  );
 }
